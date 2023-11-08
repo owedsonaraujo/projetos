116c4c6e6016970cf22ed67f4f5ee904e6dd4b91
Wedson Araújo <wedson_araujo@outlook.com>
2023-11-08 01:42:47 -0300

    Projeto do banco de dados

---
+++ target-sistemas/DbTestes/Tables/CC_EXTRATO.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CC_EXTRATO]
+(
+	[Id]             VARCHAR(36)    PRIMARY KEY,
+    [Descricao]      NVARCHAR(255) NOT NULL,
+    [Data]           DATETIME      NOT NULL DEFAULT(GETDATE()),
+    [Valor]          DECIMAL(18,2) NOT NULL,
+    [Avulso]         BIT           NOT NULL, -- Usar 1 para avulso e 0 para não avulso
+    [Status]         NVARCHAR(50)  NOT NULL CHECK (Status IN ('Válido', 'Cancelado'))
+);
